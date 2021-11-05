789e11a6b5abfa01d6b468193cf2bedfaeafbee8
David-sqrtpi <david.stiven911@gmail.com>
2021-11-04 23:35:53 -0500

    Crear los controladores de empleado

--- src/main/java/co/edu/uan/aplicacion/controladores/empleadoControlador.java
+++ src/main/java/co/edu/uan/aplicacion/controladores/empleadoControlador.java
@@ -1,6 +1,12 @@
 package co.edu.uan.aplicacion.controladores;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,14 +14,28 @@ import co.edu.uan.aplicacion.entidades.Empleado;
 
 @RestController
 public class empleadoControlador {
-	@GetMapping("/empleado")
-	public Empleado getEmpleado(@RequestBody Empleado empleado) {
-		return empleado;
+	public List<Empleado> empleados = new ArrayList<Empleado>();
+	
+	@GetMapping("/empleados")
+	public List<Empleado> getEmpleado() {
+		return empleados;
 	}
 	
-	@GetMapping("hola")
-	public String decirHola() {
-		return "Hola";
+	@PostMapping("/empleados")
+	public void crearEmpleado(@RequestBody Empleado empleado) {
+		empleados.add(empleado);
+	}
+	
+	@PutMapping("/empleados/{idEmpleado}")
+	public void actualizarEmpleado(@PathVariable long idEmpleado, @RequestBody Empleado empleado) {
+		for (Empleado empleadoActual : empleados) {
+			if(empleadoActual.getId() == idEmpleado) {
+				empleados.remove(empleadoActual);
+				break;
+			}
+		}
+		empleado.setId(idEmpleado);
+		empleados.add(empleado);
 	}
 	
 }
