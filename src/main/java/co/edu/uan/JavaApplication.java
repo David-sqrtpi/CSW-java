ea4a2aad5e49b395a17e9718506c7e868aceb227
Darwin Ricado Olave Avila <olave1907@gmail.com>
2021-11-10 23:57:12 -0500

    conexion con la base de datos

--- src/main/java/co/edu/uan/JavaApplication.java
+++ src/main/java/co/edu/uan/JavaApplication.java
@@ -14,12 +14,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import co.edu.uan.aplicacion.entidades.Empleado;
 import co.edu.uan.aplicacion.entidades.Gerencia;
 import co.edu.uan.aplicacion.entidades.Servicio;
+import conector.Conexion;
 
 @SpringBootApplication
 public class JavaApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(JavaApplication.class, args);
+		Conexion conexion = new Conexion();
+		conexion.conectar();
 	}
 
 }
