0066c6f0e181e3d34e30385a6f51bd977338574c
William Alejandro Peres Perez <wperez90@uan.edu.co>
2021-11-15 11:06:47 -0500

    borrar letras del java aplication

--- src/main/java/co/edu/uan/JavaApplication.java
+++ src/main/java/co/edu/uan/JavaApplication.java
@@ -4,13 +4,13 @@ package co.edu.uan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-<<<<<<< HEAD
-=======
+
+
 import co.edu.uan.aplicacion.entidades.Empleado;
 import co.edu.uan.aplicacion.entidades.Gerencia;
 import co.edu.uan.aplicacion.entidades.Servicio;
 import conector.Conexion;
->>>>>>> a39f21c9ce2c3fa3a000e0730f1738ecd041bcaa
+
 
 @SpringBootApplication
 public class JavaApplication {
