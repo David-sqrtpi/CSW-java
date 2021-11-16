dff26e85049bf38d4adef73abc4f8f50ab1ae9e9
Darwin Ricado Olave Avila <olave1907@gmail.com>
2021-11-16 16:55:47 -0500

    cambio inicio

--- src/main/java/conector/Conexion.java
+++ src/main/java/conector/Conexion.java
@@ -2,14 +2,19 @@ package conector;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Iterator;
+
+import com.mysql.cj.xdevapi.Statement;
 
 public class Conexion {
 	public static final String Controlador = "com.mysql.jdbc.Driver";
 	public static final String URL = "jdbc:mysql://localhost:3306/empresa";
 	public static final String USUARIO = "root";
 	public static final String CLAVE = "1234";
+    public ResultSet resultado;
 	public Connection conectar() {
 		Connection conexion = null;
 		try {
@@ -24,5 +29,6 @@ public class Conexion {
 		}
 		return conexion;
 	}
+	
 
 }
